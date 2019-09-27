6cb082390c264881fc123b42d23962548d450924
HelloGithubC <xiaoliang5@mail2.sysu.edu.cn>
2019-09-27 22:05:03 +0800

    modifide 2019.6.27-3

--- main.py
+++ main.py
@@ -15,16 +15,18 @@ if __name__=='__main__':
     cal=Calculator()
     cal.read_excel('Data.xlsx')
     m1,ST1,L_s1=cal.M_p[0],cal.ST[0],cal.L_init[0]
-    g_test=np.linspace(1e-7,1e-4,100)
-    g_in=np.ones(100)
+    cal.me.con.set_M_p(m1)
+    g=1e-8
+    ST_test=np.linspace(0,ST1,100)
     L_in=np.ones(100)
+    g_in=np.ones(100)
     index=0
-    for i in iter(g_test):
-        L_in[index],g_in[index]=cal.me.cal_ML_simple_B(ST1,L_s1,True,i)[1:3]
+    for i in iter(ST_test):
+        L_in[index],g_in[index]=cal.me.cal_ML_simple_B(i,L_s1,True,g)[1:3]
         index+=1
     fig=plt.figure(dpi=100)
     ax1=fig.add_subplot(211)
-    ax1.semilogx(g_test,g_in,'r--')
+    ax1.plot(ST_test,L_in,'r--')
     ax2=fig.add_subplot(212)
-    ax2.semilogx(g_test,L_in,'g--')
+    ax2.plot(ST_test,g_in,'g--')
     plt.savefig('test.png')
