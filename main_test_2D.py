56bfd40e8c2c8182979da65b92d4c9b46b337629
XL <xiaoliang5@mail2.sysu.edu.cn>
2019-10-07 07:04:16 +0800

    modified 2019.10.6-2

---
+++ main_test_2D.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+from numba import jit
+from calculator import Methods,Calculator
+
+
+if __name__ == "__main__":
+    pass
--- main_test_3D.py
+++ main_test_3D.py
@@ -4,8 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from numba import jit
 from calculator import Methods,Calculator
 
-def creat_plot_3D(x,y,Z,ref1,ref2,save=False):
-    X,Y=np.meshgrid(x,y)
+def creat_plot_3D(X,Y,Z,ref1,ref2,save=False):
     Z1,Z2=Z[:,:,0],Z[:,:,1]
     fig=plt.figure(dpi=100)
     ax1=plt.axes(projection='3d')
@@ -46,16 +45,13 @@ def check_values(numbers1,numbers2,ref1,ref2,error1,error2):
 if __name__ == "__main__":
     cal=Calculator()
     cal.read_excel('Data.xlsx')
-    m=cal.M_p[1]
+    order=1
+    m=cal.M_p[order]
     cal.me.con.set_M_p(m)
-    ST_max=cal.ST[1]
-    L_s=cal.L_init[1]
+    ST_max=cal.ST[order]
+    L_s=cal.L_init[order]
     ST_test=np.linspace(0,ST_max,100)
     g_out_test=np.linspace(0,1e-9,100)
     result=np.ones((100,100,2))
-    for i in range(100):
-        for j in range(100):
-            cal.me.cal_ML_simple_B(ST_test[i],L_s,True,g_out_test[j])
-            result[i,j,0]=cal.me.L[-1]
-            result[i,j,1]=cal.me.g[-1]
+    X,Y=np.meshgrid(ST_test,g_out_test)
     creat_plot_3D(ST_test,g_out_test,result,0.0,cal.me.con.g_in)
