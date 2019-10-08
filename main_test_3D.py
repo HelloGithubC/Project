7afca1b57691a9003e0c50c11aebb8827ea4367e
HelloGithubC <xiaoliang5@mail2.sysu.edu.cn>
2019-10-08 22:02:02 +0800

    modified 2019-10.8-2

--- main_test_3D.py
+++ main_test_3D.py
@@ -4,25 +4,19 @@ from mpl_toolkits.mplot3d import Axes3D
 from numba import jit
 from calculator import Methods,Calculator
 
-def creat_plot_3D(X,Y,Z,ref1,ref2,save=False):
-    Z1,Z2=Z[:,:,0],Z[:,:,1]
-    fig=plt.figure(dpi=100)
+def creat_plot_3D(X,Y,Z,ref,save=False):
+    plt.figure(dpi=100)
     ax1=plt.axes(projection='3d')
-    ax1.plot_surface(X,Y,Z1,cmap='rainbow')
-    ax1.plot_surface(X,Y,np.ones(Z1.shape)*ref1)
+    ax1.plot_surface(X,Y,Z,cmap='rainbow')
+    ax1.plot_surface(X,Y,np.ones(Z.shape)*ref)
     if save:
-        plt.savefig('result1.png')
+        if type(save) is str:
+            plt.savefig(save)
+        else:
+            plt.savefig('result.png')
     else:
         plt.show()
 
-    fig=plt.figure(dpi=100)
-    ax2=plt.axes(projection='3d')
-    ax2.plot_surface(X,Y,Z2,cmap='rainbow')
-    ax2.plot_surface(X,Y,np.ones(Z2.shape)*ref2)
-    if save:
-        plt.savefig('result2.png')
-    else:
-        plt.show()
 
 @jit(nopython=True)
 def check_values(numbers1,numbers2,ref1,ref2,error1,error2):
@@ -54,4 +48,8 @@ if __name__ == "__main__":
     g_out_test=np.linspace(0,1e-9,100)
     result=np.ones((100,100,2))
     X,Y=np.meshgrid(ST_test,g_out_test)
-    creat_plot_3D(ST_test,g_out_test,result,0.0,cal.me.con.g_in)
+    for i in range(X.shape[0]):
+        for j in range(X.shape[1]):
+            result[i,j]=cal.me.cal_ML_simple_B(X[i,j],L_s,True,Y[i,j])[1:3]
+    creat_plot_3D(X,Y,result[:,:,0],0.0)
+    creat_plot_3D(X,Y,result[:,:,1],cal.me.con.g_in)
