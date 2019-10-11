45d4b2f91e213501499ea906f96c40cb0d3f5100
HelloGithubC <xiaoliang5@mail2.sysu.edu.cn>
2019-10-11 08:31:58 +0800

    modified 2019-10.11

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__=='__main__':
-    arange=np.linspace(5.07,12,200)
+    arange=np.linspace(5.07,12,500)
     cal=Calculator()
     cal.creat_data(arange)
     fig=plt.figure(dpi=100)
     plt.plot(cal.t,cal.M_p)
-    plt.show()
+    plt.savefig('result.png')
     cal.write_excel(arange,'Data1.xlsx')
     
