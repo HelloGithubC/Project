6f38d5df6097066039c382c717619e83b1ded0a9
HelloGithubC <xiaoliang5@mail2.sysu.edu.cn>
2020-02-12 11:55:06 +0800

    deleted 2020.2.12

--- Test/test.py
+++
@@ -1,8 +0,0 @@
-from valueDataBase import DataBase
-from calculator import Calculator
-from const import Const
-from test1 import Test 
-from openpyxl import Workbook,load_workbook
-
-if __name__ == '__main__':
-    
